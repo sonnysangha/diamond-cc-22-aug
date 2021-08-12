1bbda833aaf6e3fd5ededa584b14de918d85797f
Sonny Sangha <sonny.sangha@gmail.com>
2021-08-12 19:44:10 +0100

    initial

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+function NotFound() {
+  return (
+    <div>
+      <h1>WOW You found the lost page</h1>
+    </div>
+  );
+}
+
+export default NotFound;
---
+++ pages/contact.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+function Contact() {
+  return (
+    <div>
+      <h1>Contact Me!!!</h1>
+      <Link href="/">Click to go home</Link>
+    </div>
+  );
+}
+
+export default Contact;
